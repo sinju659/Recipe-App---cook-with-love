1d04572592910fae38943a22d79a0e9ca4235b3a
Berlin Sinju <sinjugodwin@gmail.com>
2026-05-20 13:55:59 +0530

    fixed aos animation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import "aos/dist/aos.css";
 
 // https://vite.dev/config/
 export default defineConfig({
